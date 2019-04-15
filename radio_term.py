574a5d1d4f8a7993b7d6e2c7265509bab39c45ac
rasksigr <49129079+rasksigr@users.noreply.github.com>
2019-04-15 02:38:15 -0400

    Update radio_term.py

--- radio_term.py
+++ radio_term.py
@@ -88,20 +88,18 @@ while True:
 #        scribv.close()
         
         ##
-        int(width)
+        int(x)==128
         minX = -12 * len(packet_text); # 12 = 6 pixels/character * text size 2
     while True:
         display.fill(0)
-        display.text(packet_text, width, 0, 1)
-        #display.print(packet_text)
+        display.text(packet_text, x, 0, 1)
         display.show()
         x = x-1
-        if x > minX: 
-            True
+        if x > minX: True
         else: False
 
         ##
-        display.text("packet_text", 20, 0, 1)
+        display.text(packet_text, 20, 0, 1)
         display.show()
         display.scroll(0, 15)
         time.sleep(1)
